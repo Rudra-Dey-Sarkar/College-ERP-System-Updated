23083db75756757bc8727f6f52a600b7d27bab25
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-06-25 13:44:45 +0530

    Update FileUpload.js

--- Main/Frontend/src/Components/FileUpload/FileUpload.js
+++ Main/Frontend/src/Components/FileUpload/FileUpload.js
@@ -8,7 +8,7 @@ export default function () {
         <div className='uploads'>
         <div className='notice'>
         <h3>Upload Notice</h3>
-    <form action="http://localhost:5001/upload-notice" method="post" enctype="multipart/form-data">
+    <form action="https://college-erp-system-updated.vercel.app/upload-notice" method="post" enctype="multipart/form-data">
       <input type="file" name="pdfnoticeupload"></input>
       <button type='submit'>Upload</button>
     </form>
@@ -16,7 +16,7 @@ export default function () {
 
       <div className='attendance'>
         <h3>Upload Attendance</h3>
-    <form action="http://localhost:5001/upload-attendance" method="post" enctype="multipart/form-data">
+    <form action="https://college-erp-system-updated.vercel.app/upload-attendance" method="post" enctype="multipart/form-data">
       <input type="file" name="pdfattendanceupload"></input>
       <button type='submit'>Upload</button>
     </form>
