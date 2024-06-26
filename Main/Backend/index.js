c32f790f0c923ecf1a956c7c1df2f56736ea45c9
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-06-26 13:23:10 +0530

    Update index.js

--- Main/Backend/index.js
+++ Main/Backend/index.js
@@ -20,7 +20,7 @@ mongoose.connect(dbURI,
 const app = express();
 app.use(express.json());
 app.use(cors({
-origin: ["https://college-erp-system-update-git-9a3b61-rudra-dey-sarkars-projects.vercel.app/"],
+origin: ["https://college-erp-system-updated-frontend.vercel.app"],
 method: ["POST", "GET", "PUT", "DELETE"],
 credentials: true
 }));
