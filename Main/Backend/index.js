19a6b827f5a857f5d5af28457a5432f21ad6fec3
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-07-31 19:45:19 +0530

    Update index.js

--- Main/Backend/index.js
+++ Main/Backend/index.js
@@ -20,7 +20,7 @@ mongoose.connect(dbURI,
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["https://college-erp-system-updated-frontend.vercel.app/adminlogin"], // Add your frontend URL here
+    origin: ["https://college-erp-system-updated-frontend.vercel.app"], // Add your frontend URL here
   //  origin: true,
     methods: ["POST", "GET", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
